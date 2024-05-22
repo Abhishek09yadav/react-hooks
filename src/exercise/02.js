e43d4f3c1b49a92c148e8e2a967339eff3350049
Abhishek Yadav <monuy8830@gmail.com>
2024-05-22 10:22:43 +0530

    learning hooks

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,10 +3,16 @@
 
 import * as React from 'react'
 
+// eslint-disable-next-line no-use-before-define
 function Greeting({initialName = ''}) {
+  const [name, setName] = React.useState(window.localStorage.getItem('name') || initialName)
+
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
+  React.useEffect(() => {
+    console.log('useEffect called')
+    window.localStorage.setItem('name', name)
+  }, [name])
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
@@ -14,20 +20,24 @@ function Greeting({initialName = ''}) {
 
   function handleChange(event) {
     setName(event.target.value)
+    window.localStorage.setItem('name', event.target.value)
   }
-  return (
-    <div>
-      <form>
-        <label htmlFor="name">Name: </label>
-        <input value={name} onChange={handleChange} id="name" />
-      </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
-    </div>
-  )
+
+  return (<div>
+    <form>
+      <label htmlFor="name">Name: </label>
+      <input value={name} onChange={handleChange} id="name" />
+    </form>
+    {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+  </div>)
 }
 
 function App() {
-  return <Greeting />
+  const [count, setcount] = React.useState(0)
+
+  return <>
+    <button onClick={() => setcount(p => p + 1)}>click {count}</button>
+    <Greeting /></>
 }
 
 export default App
